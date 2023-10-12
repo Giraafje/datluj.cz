597887291bf3c7ecc48a947cf4b1fba698de61aa
Giraafje <lenka.erbenova79@gmail.com>
2023-10-12 22:25:56 +0200

    Not working yet

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
+const Wordbox = ({ word, onFinish, active }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);
+  const [mistake, setMistake] = useState(false)
+  
+  useEffect(() => {
+    const handleKeyUp = (e) => {
+      if (lettersLeft) {
+        if (e.code === `Key${lettersLeft[0].toUpperCase()}`) {
+          setLettersLeft((oldWord) => oldWord.slice(1))
+          setMistake(false)
+        } else {
+          setMistake(true)
+        }
+      } else {
+        onFinish()
+      }
+      }      
+    if (active) {document.addEventListener('keyup', handleKeyUp)};
+    return () => {if (active) {document.removeEventListener('keyup', handleKeyUp)}};
+  }, [lettersLeft, active]);
   
   return (
-    <div className="wordbox">{lettersLeft}</div>
+    <div className={mistake ? "wordbox--mistake" : "wordbox"}>{lettersLeft}</div>
   );
 };
 
